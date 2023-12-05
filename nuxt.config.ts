8bbb1e24ae8d56096bae4d5933d4208259afc21c
Carlos Soriano <soriano.carlitos@gmail.com>
2023-12-05 09:11:21 -0500

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,71 +1,74 @@
 export default defineNuxtConfig({
-    pwa: {
-        icon: {
-            fileName: 'android-chrome-512x512.png',
-        },
-        manifest: {
-            background_color: '#f5f5f5',
-            name: 'Golem',
-            categories: ['productivity', 'education'],
-            description: 'Golem is an open-source conversational UI and alternative to ChatGPT',
-            display: 'standalone',
-            lang: 'en',
-            id: `golem-${new Date().getTime()}`,
-            theme_color: '#3f3f3f',
-        },
+  pwa: {
+    icon: {
+      fileName: "android-chrome-512x512.png",
     },
+    manifest: {
+      background_color: "#f5f5f5",
+      name: "Golem",
+      categories: ["productivity", "education"],
+      description:
+        "Golem is an open-source conversational UI and alternative to ChatGPT",
+      display: "standalone",
+      lang: "en",
+      id: `golem-${new Date().getTime()}`,
+      theme_color: "#3f3f3f",
+    },
+  },
 
-    css: ['~/assets/css/main.css'],
-    experimental: {
-        reactivityTransform: true,
+  css: ["~/assets/css/main.css"],
+  experimental: {
+    reactivityTransform: true,
+  },
+  modules: [
+    "@unocss/nuxt",
+    "@vueuse/nuxt",
+    "@nuxtjs/color-mode",
+    "@kevinmarrec/nuxt-pwa",
+  ],
+  colorMode: {
+    classSuffix: "",
+  },
+  unocss: {
+    attributify: true,
+    uno: true,
+    icons: true,
+    webFonts: {
+      fonts: {
+        code: "DM Mono:400",
+        text: "Rubik:400,700",
+        title: "Space Grotesk:400,700",
+      },
     },
-    modules: [
-        '@unocss/nuxt',
-        '@vueuse/nuxt',
-        '@nuxtjs/color-mode',
-        '@kevinmarrec/nuxt-pwa',
-    ],
-    colorMode: {
-        classSuffix: '',
+  },
+  vite: {
+    define: {
+      "process.env.VSCODE_TEXTMATE_DEBUG": "false",
+      __DEV__: process.env.NODE_ENV === "development",
     },
-    unocss: {
-        attributify: true,
-        uno: true,
-        icons: true,
-        webFonts: {
-            fonts: {
-                code: 'DM Mono:400',
-                text: 'Rubik:400,700',
-                title: 'Space Grotesk:400,700',
-            },
-        },
+  },
+  hooks: {
+    "vite:extendConfig": function (config, { isServer }) {
+      if (isServer) {
+        const alias = config.resolve!.alias as Record<string, string>;
+        for (const dep of ["shiki-es", "fuse.js"]) {
+          alias[dep] = "unenv/runtime/mock/proxy";
+        }
+      }
     },
-    vite: {
-        define: {
-            'process.env.VSCODE_TEXTMATE_DEBUG': 'false',
-            '__DEV__': process.env.NODE_ENV === 'development',
-        },
-
-    },
-    hooks: {
-        'vite:extendConfig': function (config, { isServer }) {
-            if (isServer) {
-                const alias = config.resolve!.alias as Record<string, string>
-                for (const dep of ['shiki-es', 'fuse.js']) {
-                    alias[dep] = 'unenv/runtime/mock/proxy'
-                }
-            }
-        },
-    },
-    app: {
-        pageTransition: { name: 'page', mode: 'out-in' },
-    },
-    runtimeConfig: {
-        detaKey: process.env.DETA_PROJECT_KEY,
-        apiKey: process.env.OPENAI_API_KEY,
-        password: process.env.GOLEM_PASSWORD,
-    },
-    build: {
-        transpile: ['trpc-nuxt', 'dexie'],
-    },
-})
+  },
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+  },
+  runtimeConfig: {
+    detaKey: process.env.DETA_PROJECT_KEY,
+    apiKey: process.env.OPENAI_API_KEY,
+    password: process.env.GOLEM_PASSWORD,
+  },
+  build: {
+    transpile: ["trpc-nuxt", "dexie"],
+  },
+  nitro: {
+    preset: "cloudflare",
+  },
+});
